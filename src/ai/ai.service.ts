734179aac3060e9ab39f1ba53532eb02d788a241
alexl-shopware <alex.lebron@shop-ware.com>
2023-04-22 23:47:30 -0400

    feature/ai-ai: First hookup to openai in the ai service.

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { randomUUID } from 'crypto';
 import { isUUID } from 'src/utils';
 import { Repository } from 'typeorm';
+import openai from './openai_config';
 
 @Injectable()
 export class AiService {
@@ -10,6 +11,18 @@ export class AiService {
   INVALID_UUID = 'Invalid UUID';
 
   async getAiRes() {
-    return 'Ai is thinking...';
+    try {
+      console.log('Getting AI response');
+      const res = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: 'Say this is a test',
+        temperature: 0,
+        max_tokens: 7,
+      });
+      console.log('AI res:', res?.data?.choices[0]?.text);
+      return res?.data?.choices[0]?.text;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
